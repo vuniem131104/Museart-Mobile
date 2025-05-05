d699c6e5aafca2857caf6af7cfa78abfd44dbae4
HinmQ21 <leminh2112k4@gmail.com>
2025-05-05 20:43:14 +0700

    chore: url

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,3 +1,6 @@
 export const baseUrl = 'https://api.artic.edu/api/v1';
-export const localhost = 'http://192.168.1.11:8001';
-export const backendUrl = 'https://84b0-2402-800-61a0-f569-fc8-a759-3f5d-35f6.ngrok-free.app/api';
+
+
+const LOCAL_IP = '192.168.0.149'; // Địa chỉ IP máy dev của bạn
+export const localhost = `http://${LOCAL_IP}:8001`;
+export const backendUrl = `http://${LOCAL_IP}:3000/api`;
